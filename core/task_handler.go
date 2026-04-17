b98e8ba3124d4e494d3a8c44f5b44dd53ba2b07b
Tim <ttsutskarev@gmail.com>
2026-04-17 18:36:49 +0300

    move server logic to core

--- core/task_handler.go
+++ core/task_handler.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -31,6 +32,34 @@ func HttpHandler[InputSchema any](
 		method,
 		_handler,
 	}
+}
+func New(handlers ...Handler) http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		writeJSON(w, 200, map[string]any{
+			"healthy": true,
+		})
+	})
+
+	for _, h := range handlers {
+		mux.HandleFunc(h.Method, h.HandlerFunc)
+	}
+
+	return mux
+}
+
+func parseRequest(r *http.Request) (ListingInput, error) {
+	var input ListingInput
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		return ListingInput{}, fmt.Errorf("Invalid payload")
+	}
+	if err := input.Validate(); err != nil {
+		return ListingInput{}, err
+	}
+	return input, nil
 }
 
 func writeJSON(w http.ResponseWriter, status int, v any) {
